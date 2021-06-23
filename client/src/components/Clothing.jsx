b777dc4272c0bf6841312d77ce78c9d737a2920a
Jared Absher <jared.absher@gmail.com>
2021-06-23 16:58:43 -0400

    adding table to clothing component

--- client/src/components/Clothing.jsx
+++ client/src/components/Clothing.jsx
@@ -1,5 +1,24 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
+import { lighten, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FilterListIcon from '@material-ui/icons/FilterList';
 import { GearContext } from '../GearContext';
 const Clothing = (props) => {
   const [num, setNum] = useState(3);
@@ -8,6 +27,10 @@ const Clothing = (props) => {
   const [bringing, setBringing] = useState('no');
   const [inhand, setInhand] = useState('no');
   const [packed, setPacked] = useState('no');
+
+  function createData (Item, Priority, Description, Example, Bringing, Inhand, Packed) {
+    return {Item, Priority, Description, Example, Bringing, Inhand, Packed};
+  }
 
   const handleInhandClick = (e) => {
     setInhand('yes');
