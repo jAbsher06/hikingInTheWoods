f1319e19c13eb62d3c996be94909b2bff6866a81
Jared Absher <jared.absher@gmail.com>
2021-07-19 17:10:50 -0400

    added custom table using react table

---
+++ client/src/components/ClothingTable.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import { lighten, makeStyles } from '@material-ui/core/styles';
+import MaUTable from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { useTable } from 'react-table';
+import { GearContext } from '../GearContext';
+
+const ClothingTable = ({columns, data}) => {
+  const { getTableProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data,
+  })
+
+  return (
+    <MaUTable {...getTableProps()}>
+      <TableHead>
+        {headerGroups.map(headerGroup => (
+          <TableRow {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <TableCell {...column.getHeaderProps()}>
+                {column.render('Header')}
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableHead>
+      <TableBody>
+        {rows.map((rows, i) => {
+          prepareRow(row)
+          return (
+            <TableRow {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return (
+                  <TableCell {...cell.getCellProps()}>
+                    {cell.render('Cell')}
+                  </TableCell>
+                )
+              })}
+            </TableRow>
+          )
+        })}
+      </TableBody>
+    </MaUTable>
+  )
+}
+
+export default CLothingTable;
